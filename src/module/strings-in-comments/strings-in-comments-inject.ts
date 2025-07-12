0f54b1738256b00eb3e7283ce97d937bda01985b
Bernardo Lopes <steamloc@berna.zone>
2025-07-12 16:35:06 -0300

    Implement Language-Agnostic option in the context menu for Translate mode.

--- src/module/strings-in-comments/strings-in-comments-inject.ts
+++ src/module/strings-in-comments/strings-in-comments-inject.ts
@@ -1,6 +1,8 @@
 import {ExtensionMessageId} from "./aux-objects/extension-message";
 import {CrowdinPhrasesResponse} from "../../apis/crowdin/multiple-phrases/crowdin-phrases-response";
-import {listenToExtensionMessage} from "../../util/util";
+import {listenToExtensionMessage, observeElementEvenIfNotReady} from "../../util/util";
+
+let altDown = false;
 
 const updateStringUrlInterval = setInterval(() => {
     //@ts-ignore
@@ -11,7 +13,7 @@ const updateStringUrlInterval = setInterval(() => {
 
 
         //@ts-ignore
-        window.crowdin.helpers.translation.getStringUrl = (id: number): string => {
+        window.crowdin.helpers.translation.getStringUrl = (id: number, languageless: boolean = false): string => {
             //@ts-ignore
             const url = new URL(originalGetStringUrl.apply(window.crowdin.helpers.translation, [id]));
             const stringId = parseInt(url.hash.replace("#", ""));
@@ -24,6 +26,11 @@ const updateStringUrlInterval = setInterval(() => {
                     }
                 }
             }
+            if (languageless) {
+                let splitPath = url.pathname.split("/");
+                let languageless = splitPath.toSpliced(4);
+                url.pathname = languageless.join("/")
+            }
             return url.toString();
         }
         clearInterval(updateStringUrlInterval);
@@ -32,6 +39,89 @@ const updateStringUrlInterval = setInterval(() => {
 }, 500);
 
 let phrases: CrowdinPhrasesResponse = null;
+
+function altStuff(e: KeyboardEvent, button: HTMLElement) {
+    if (e.altKey) {
+        altDown = true;
+        button.textContent = "Copy Language-Agnostic String URL"
+    } else {
+        altDown = false;
+        button.textContent = "Copy String URL";
+    }
+}
+
+const insertCopyWithoutLanguageForCurrentStringInterval = setInterval(() => {
+    const buttonElement = document.querySelector("#string-options-link");
+    const listElement = document.querySelector("#file_options");
+    // @ts-ignore
+    if (window.crowdin && window.crowdin.helpers && window.crowdin.helpers.translation && buttonElement && listElement) {
+        clearInterval(insertCopyWithoutLanguageForCurrentStringInterval);
+        const regularCopyButton = [...listElement.querySelectorAll("li")]
+            .find(e => e.textContent === "Copy String URL");
+        const languagelessCopy = document.createElement("li");
+        regularCopyButton.after(languagelessCopy);
+        const languagelessCopyAnchor = document.createElement("a");
+        languagelessCopy.append(languagelessCopyAnchor);
+        languagelessCopyAnchor.append("Copy Language-Agnostic URL");
+        languagelessCopyAnchor.tabIndex = -1;
+        languagelessCopyAnchor.href = "#";
+        languagelessCopyAnchor.addEventListener("click", (e) => {
+            // @ts-ignore
+            crowdin.clipboard.successMessage = "Language-Agnostic URL copied to clipboard!";
+            // @ts-ignore
+            crowdin.clipboard.copy(crowdin.helpers.translation.getStringUrl(null, true));
+            e.preventDefault();
+        })
+    }
+}, 500)
+
+const insertCopyWithoutLanguageForStringListInterval = setInterval(() => {
+    const listElement = document.querySelector("#source-string-context-menu");
+    // @ts-ignore
+    if (window.crowdin && window.crowdin.helpers && window.crowdin.helpers.translation && window.crowdin.editor && listElement && listElement) {
+        clearInterval(insertCopyWithoutLanguageForStringListInterval);
+        // @ts-ignore
+        if (crowdin.editor.modeTranslate()) {
+            new MutationObserver((m, o) => {
+                const button = listElement.querySelector(".contexify_item[data-testid='copyStringUrl']");
+
+                const newButton = document.createElement("div");
+                button.after(newButton);
+                newButton.classList.add("contexify_item");
+                newButton.tabIndex = -1;
+                newButton.ariaDisabled = "false";
+                newButton.role = "menuitem";
+
+                const newButtonLabel = document.createElement("div");
+                newButton.append(newButtonLabel);
+                newButtonLabel.classList.add("contexify_itemContent");
+                newButtonLabel.textContent = "Copy Language-Agnostic URL";
+
+                newButton.addEventListener("click", (e) => {
+                    const element = Array.from(document.querySelectorAll("li.context-menu")).find(e => e.id.startsWith("phrase_"));
+                    if (element) {
+                        const id = parseInt(element.id.replace("phrase_", ""))
+                        // @ts-ignore
+                        crowdin.clipboard.copy(crowdin.helpers.translation.getStringUrl(id, true))
+                    }
+                })
+            })
+                .observe(listElement, {childList: true})
+        }
+    }
+}, 500)
+
+/*
+            function b(e) {
+              var t = e.props;
+              (
+                crowdin.editor.modeTranslate() ||
+                1 === crowdin.phrases.get_selected_translations_id().length
+              ) &&
+              crowdin.clipboard.copy(crowdin.helpers.translation.getStringUrl(t.translation.id))
+            }
+
+ */
 
 $(document).ajaxSuccess((_e: JQuery.TriggeredEvent, _xhr: JQuery.jqXHR, options: JQuery.AjaxSettings, data: JQuery.PlainObject) => {
     if (options.url === ("/backend/phrases") && (options.type === "POST" || options.method === "POST")) {
